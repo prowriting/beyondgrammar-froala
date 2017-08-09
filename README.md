91d4f626f8c921a91594cb9e3b18377d5ee9c543
orpheustech <chris.daniel.banks@gmail.com>
2017-08-09 11:50:35 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# realtimegrammar-froala
-Bring real-time spelling, grammar and style checking into your Froala editor. Perfect for web CMS systems, help desk systems and blogs
+# Real-time Grammar plugin for Froala editor
+Bring real-time spelling, grammar and style checking into your Froala editor. Perfect for web CMS systems, help desk systems and blogs.
+
+To add real-timegrammar, spell and style checking to your Froala editor you just need to install this add-in. It's free for individuals and reasonably proced for multi-user licenses.
+
+##Why choose our plugin?
+Our state-of-the-art grammar checker is used by over 500,000 users. It contains many unique features that you won't find in other solutions. These include:
+
+- Entity spellchecking. We check over 2 million people, places, teams, towns and other terms. It highlights where you may have incorrectly spelled a name. e.g. Andy Murrey => Andy Murray
+
+- Contextual spelling. We use artificial intelligence to highlight where you may have used a word in the incorrect context. It's easy to mistype a word, or slip a homonym in the wrong place, but we will highlight these, e.g. *He is my best `fiend` in all the world.* or *I love my `knew` shoes.* Most other grammar checkers just use simple rules than only catch a fraction of possible mistakes.
+
+- Style checking. We include over 15,000 potential style improvements for you text to ensure that it's not only grammatically correct, but also well-written and punchy.
+
+- Contextual thesaurus. Our contextual thesaurus looks at the context of the word you want to look up. It then limits the suggestions to just those so you can quickly pick the right synonym. There's also a full thesaurus option if you prefer that.
+
+
---
+++ src/realtime-grammar-plugin.ts
@@ -0,0 +1,246 @@
+require('style!css!./styles/froala-plugin-styles.css');
+
+(($)=>{
+    
+    let settings = {
+        service : {
+            i18n : { en : "./libs/i18n-en.js" },
+            sourcePath : "https://prowriting.azureedge.net/realtimegrammar/1.0.65/dist/bundle.js",
+            userId : null,
+            apiKey : null,
+            serviceUrl: "//rtg.prowritingaid.com"
+        },
+        grammar : {
+            languageFilter: null,
+            languageIsoCode: null,
+            checkStyle: true,
+            checkSpelling: true,
+            checkGrammar: true,
+        },
+        hideDisable: false
+    };
+
+    let getOptionsHtml = ()=>{
+        if (!checker) {
+            return '<ul class="fr-dropdown-list" role="presentation"><li role="presentation"><a class="fr-command" tabindex="-1" role="option" data-cmd="rtg-switcher" data-param1="v1" title="Option 1" aria-selected="false">Option 22354</a></li><li role="presentation"><a class="fr-command" tabindex="-1" role="option" data-cmd="rtg-switcher" data-param1="v2" title="Option 2" aria-selected="false">Option 2</a></li></ul>';
+        }
+
+        var html = '<ul class="fr-dropdown-list" role="presentation">';
+        var languages = checker
+            .getAvailableLanguages();
+        if (languages) {
+            languages.filter((elem)=> {
+                if (!settings.grammar.languageFilter) {
+                    return true;
+                } else {
+                    console.log(settings.grammar.languageFilter);
+                    if (settings.grammar.languageFilter.indexOf(elem.isoCode) >= 0) {
+                        return true;
+                    }
+                }
+                return false;
+            }).forEach((lang)=> {
+                var bullet = lang.isoCode == language ? ' •' : '';
+                html += '<li role="presentation"><a class="fr-command" tabindex="-1" role="option" data-cmd="rtg-switcher" data-param1="' + lang.isoCode + '" title="' + lang.displayName + '" aria-selected="false">' + lang.displayName + bullet + '</a></li>';
+            });
+        }else{
+            console.log('No languages available');
+        }
+
+        if (!settings.hideDisable) {
+            // add a disable checking option
+            var offBullet = "off" == language ? ' •' : '';
+            html += '<li role="presentation"><a class="fr-command" tabindex="-1" role="option" data-cmd="rtg-switcher" data-param1="off" title="No Checking" aria-selected="false">No Checking' + offBullet + '</a></li>';
+        }
+        html+='</ul>'
+        return html;
+
+    };
+
+    window.addEventListener(
+        "pwa-language-change",
+        (event) => {
+            var settings = checker.getSettings();
+            // clone the settings
+            settings = JSON.parse(JSON.stringify(settings));
+            settings.languageIsoCode=(<any>event).detail.language;
+            checker.setSettings(settings);
+
+            },
+        false);
+
+    // get the default language from the browser
+    // or from a cookie
+    let browserLanguage = (<any>window.navigator).userLanguage || window.navigator.language;
+    let language = browserLanguage=="en-GB"?'en-GB':'en-US';
+    let checker = null;
+
+    $.FroalaEditor.DefineIconTemplate('rtg', '<i class="rtg-toolbar-icon"></i>');
+    $.FroalaEditor.DefineIcon('rtg-icon', { NAME: 'icon', template : "rtg"});
+    $.FroalaEditor.RegisterCommand('rtg-switcher', {
+        title: 'Real-time Grammar Checking',
+        type: 'dropdown',
+        icon: 'rtg-icon',
+        focus: false,
+        undo: false,
+        refreshAfterCallback: true,
+        html: ()=>{
+            return getOptionsHtml();
+        },
+        callback: function (cmd, val) {
+
+        },
+        // Callback on refresh.
+        refresh: function ($btn) {
+            //console.log ('do refresh');
+        },
+        // Callback on dropdown show.
+        refreshOnShow: function ($btn, $dropdown) {
+            console.log ('do refresh when show');
+            var editorInstance = this,
+                list = $dropdown.find('ul.fr-dropdown-list');
+
+            $(list).html(getOptionsHtml());
+        }
+    });
+	
+    $.FroalaEditor.PLUGINS.RealtimeGrammarPlugin = function (editor){
+        let states = [ "loading", "connected", "disconnected", "off" ];        
+        let labelsByState = {
+            "loading" : "Real-time Grammar is loading",
+            "connected" : "Real-time Grammar is online (click to change language)",
+            "disconnected" : "Real-time Grammar is offline (click to start)",
+            "off" : "Real-time Grammar is stopped (click to change language)"
+        };
+        
+        let plugin = {
+            state : "",
+            checker : null,
+            
+            _init : ()=>{
+                console.log (editor.opts.myOption);
+                if (editor.opts){
+                    if (editor.opts.rtgOptions){
+                        settings.grammar = editor.opts.rtgOptions;
+                    }
+                    if (!settings.grammar.languageIsoCode){
+                        settings.grammar.languageIsoCode = language;
+                        console.log('Defaulting checking language to: '+language)
+                    }
+                    if (editor.opts.rtgSourcePath){
+                        settings.service.sourcePath = editor.opts.rtgSourcePath;
+                    }
+                    if (editor.opts.rtgUserId){
+                        settings.service.userId = editor.opts.rtgUserId;
+                    }
+                    if (editor.opts.rtgApiKey){
+                        settings.service.apiKey = editor.opts.rtgApiKey;
+                    }
+                    if (editor.opts.rtgServiceUrl){
+                        settings.service.serviceUrl= editor.opts.rtgServiceUrl;
+                    }
+                    if (editor.opts.rtgDisableHidden){
+                        settings.hideDisable = editor.opts.rtgDisableHidden;
+                    }
+                }
+
+                plugin.setState("loading");
+                plugin.loadScript(settings.service.sourcePath, ()=>{
+                    plugin.activate();
+                });
+            },
+            
+            onRefreshButton : ($btn)=>{
+                $btn.find(".rtg-toolbar-icon").removeClass(states.join(" ")).addClass(plugin.state);
+                $btn.data("title", labelsByState[plugin.state]);
+            },
+            
+            onToolbarButtonClick : ()=>{
+                if( plugin.state == "loading" ) {
+                    return;
+                }
+
+                if( plugin.state != "off" ) {
+                    plugin.deactivate();
+                } else {
+                    plugin.activate();
+                }
+            },
+
+            onLanguageOptionClick : (cmd, val)=>{
+                if( plugin.state == "loading" ) {
+                    return;
+                }
+
+                if( val == "off" ) {
+                    var settings = plugin.checker.getSettings();
+                    // clone the settings
+                    settings = JSON.parse(JSON.stringify(settings));
+                    settings.checkerIsEnabled=false;
+                    plugin.checker.setSettings(settings);
+                } else {
+                    if( plugin.state == "off" ) {
+                        plugin.activate();
+                    }
+                    var settings = plugin.checker.getSettings();
+                    // clone the settings
+                    settings = JSON.parse(JSON.stringify(settings));
+                    settings.languageIsoCode=val;
+                    settings.checkerIsEnabled=true;
+                    plugin.checker.setSettings(settings);
+                }
+                console.log('Settings language to: '+val);
+                language = val;
+
+                // Create the event.
+                var event = document.createEvent('CustomEvent');
+
+				// Define that the event name is 'build'.
+                event.initCustomEvent('pwa-language-change', true, true, {
+                    language: val
+                });
+
+				// target can be any Element or other EventTarget.
+                window.dispatchEvent(event);
+            },
+
+            activate : ()=>{
+                plugin.setState("loading");
+
+                plugin.checker = new window["Pwa"].GrammarChecker(editor.$el[0], settings.service);
+
+                plugin.checker.setSettings(settings.grammar);
+                checker = plugin.checker;
+                
+                plugin.checker.onConnectionChange = (status)=>{
+                    plugin.setState(status);
+                };
+                
+                plugin.checker.init().then(()=>plugin.checker.activate());
+            },
+            
+            deactivate : ()=>{
+                plugin.checker.deactivate();
+                plugin.checker.onConnectionChange = null;
+                plugin.setState("off");
+            },
+            
+            setState : ( state : string )=>{
+                plugin.state = state;
+                editor.button.bulkRefresh();
+            },
+            
+            loadScript : (src, onComplete)=>{
+                let script = document.createElement("script");
+                script.onload = onComplete || (()=>console.log(`"${src}" was loaded`));
+                script.src = src;
+                document.body.appendChild(script);
+            }
+        };
+
+        $.FroalaEditor.COMMANDS["rtg-switcher"].callback = (cmd,val)=>plugin.onLanguageOptionClick(cmd,val);
+        $.FroalaEditor.COMMANDS["rtg-switcher"].refresh = ($btn)=>plugin.onRefreshButton($btn);
+        
+        return plugin
+    };
+})(window["jQuery"]);
