45a9490ff21c1105892d712b532703ba2f254727
Chris Dinsmore <c.dinsmore@gmail.com>
2017-08-15 10:12:17 +0100

    Updated RTG version

--- src/realtime-grammar-plugin.ts
+++ src/realtime-grammar-plugin.ts
@@ -5,7 +5,7 @@ require('style!css!./styles/froala-plugin-styles.css');
     let settings = {
         service : {
             i18n : { en : "./libs/i18n-en.js" },
-            sourcePath : "//prowriting.azureedge.net/realtimegrammar/1.0.94/dist/bundle.js",
+            sourcePath : "//prowriting.azureedge.net/realtimegrammar/1.0.95/dist/bundle.js",
             userId : null,
             apiKey : null,
             serviceUrl: "//rtg.prowritingaid.com"
